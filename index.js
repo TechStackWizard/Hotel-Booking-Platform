e46273908781af90130f4dc582dae8b3a8e03ecb
TechStackWizard <anshullko474@gmail.com>
2025-05-24 15:22:26 +0530

    Add Authentication method using passport npm package

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate')
 const session = require('express-session');
 const flash = require('connect-flash')
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js')
+
 
 const ExpressError = require('./utils/ExpressError.js')
 const listingRoutes = require('./routes/listing.js'); // Assuming the routes are in a folder named 'routes'
@@ -56,10 +60,27 @@ const sessionOptions = {
 app.use(session(sessionOptions))
 app.use(flash())
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+app.get('/registeruser', async(req, res) => {
+    let fakeUser = new User({
+        email: 'abc@gmail.com',
+        username: 'abc',
+    });
+
+    let registeredUser = await User.register(fakeUser, 'abcpass');
+    res.send(registeredUser);
 })
 
 app.use('/listings', listingRoutes);
